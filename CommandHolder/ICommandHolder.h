2295f2c2c170fa2e0ff6da61e9d6c76555b905e7
Sergey Firsov <fearthunder@yandex.ru>
2020-09-01 21:06:21 +0300

    Add command construction

--- CommandHolder/ICommandHolder.h
+++ CommandHolder/ICommandHolder.h
@@ -1,19 +1,30 @@
 #pragma once
 
 #include "../Commands/ICommand.h"
+#include "../DataTypes/CommandData.h"
 
 #include <string>
 #include <vector>
+#include <functional>
 
 class ICommandHolder
 {
+protected:
+
+    virtual void AddNewCommand(std::string commandName, std::function<ICommand*(CommandArguments arguments)>) = 0;
+
 public:
 
-    template<class ... CommandArguments>
-    void AddCommand()
+    template<class CommandClass, class ... CommandArguments>
+    void AddCommand(const std::string& commandName, CommandArguments... args)
     {
+        auto& commandConstructor = [commandName, args...]()
+        {
+            return new CommandClass(commandName, args...);
+        };
 
+        AddNewCommand(commandName, commandConstructor);
     }
 
-    virtual ICommand GetCommand(std::string commandName) = 0;
+    virtual ICommand* GetCommand(std::string commandName) = 0;
 };
