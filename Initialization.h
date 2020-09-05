6606c5d37d291671f8e3ee90015e389fb4abf13c
Sergey Firsov <fearthunder@yandex.ru>
2020-09-05 19:58:02 +0300

    Add init.h file

---
+++ Initialization.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <memory>
+
+#include "Parser/Parser.h"
+#include "CommandHolder/CommandHolder.h"
+#include "Executor/Executor.h"
+#include "Coder/Coder.h"
+
+#include "Commands/CommandVersion.h"
+#include "Commands/CommandEncode.h"
+#include "Commands/CommandDecode.h"
+
+static std::shared_ptr<IParser> Parser {new class Parser()};
+static std::shared_ptr<ICommandHolder> CommandHolder {new class CommandHolder()};
+static std::shared_ptr<IExecutor> Executor {new class Executor(CommandHolder.get())};
+static std::shared_ptr<ICoder> Coder {new class Coder()};
+
+void Init()
+{
+    CommandHolder->AddCommand<CommandVersion>("version", "0.1a");
+    CommandHolder->AddCommand<CommandEncode>("encode", Coder.get());
+    CommandHolder->AddCommand<CommandDecode>("decode", Coder.get());
+}
--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
-#include <memory>
-
-#include "Parser/Parser.h"
-#include "CommandHolder/CommandHolder.h"
-#include "Executor/Executor.h"
-#include "Coder/Coder.h"
-
-#include "Commands/CommandVersion.h"
-#include "Commands/CommandEncode.h"
-#include "Commands/CommandDecode.h"
-
+#include "Initialization.h"
 
 int main(int argc, char *argv[])
 {
-    std::shared_ptr<IParser> Parser {new class Parser()};
-    std::shared_ptr<ICommandHolder> CommandHolder {new class CommandHolder()};
-    std::shared_ptr<IExecutor> Executor {new class Executor(CommandHolder.get())};
-    std::shared_ptr<ICoder> Coder {new class Coder()};
-
-    CommandHolder->AddCommand<CommandVersion>("version", "0.1a");
-    CommandHolder->AddCommand<CommandEncode>("encode", Coder.get());
-    CommandHolder->AddCommand<CommandDecode>("decode", Coder.get());
+    Init();
 
     Parser->Parse(argc, argv);
     Executor->ExecuteCommands(Parser->GetParsedCommands());
